d4f94a88c598b98ccbce1477690f009ab7e2fbb3
udyThe <udaysurothiya@gmail.com>
2025-12-24 00:30:03 +0530

    run demo enhancement after seeding

--- laravel-api/seed.js
+++ laravel-api/seed.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
+const { spawn } = require('child_process');
 const database = require('./database');
 
 console.log('Article Seeder - Importing scraped articles');
